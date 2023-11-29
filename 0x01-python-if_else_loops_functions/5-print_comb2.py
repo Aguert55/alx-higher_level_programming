89616bf01529bbc927c51dda0e6136b0d9696fad
Aguert55 <oussamaaguert5@gmail.com>
2023-11-29 03:55:36 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number  in range(100):
+for number in range(0, 100):
     if number == 99:
-        print(number)
+        print("{}".format(number))
     else:
-        print("{:0>2d}".format(number), end=", ")
+        print("{:02}".format(number), end=", ")
