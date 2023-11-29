f1b89f7416004d91ea29c2f600d7f3e4ba1741a9
Aguert55 <oussamaaguert5@gmail.com>
2023-11-28 16:56:12 -0800

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 import random
+
+# Assign a random signed number to the variable number
 number = random.randint(-10000, 10000)
+
+# Print the number
+print(f'The number is: {number}')
+
+# Extract the last digit of the number
 last_digit = abs(number) % 10
-if number < 0:
-    digit = -digit
+
+# Print the last digit of the number
 print(f'Last digit of {number} is {last_digit}', end=" ")
+
+# Check conditions based on the last digit
 if last_digit > 5:
     print('and is greater than 5')
 elif last_digit == 0:
